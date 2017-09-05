d17e16b36942feea38951a692a57abd6850f6f51
mmoanis <mohamed_moanis@outlook.com>
2017-09-05 21:11:33 +0200

    Turn off Window rendering for now

--- src/render.cc
+++ src/render.cc
@@ -2,16 +2,6 @@
 
 Renderer::Renderer()
 {
-    sf::ContextSettings contextSettings;
-        contextSettings.depthBits = 24;
-        contextSettings.stencilBits = 8;
-        contextSettings.antialiasingLevel = 0;
-        contextSettings.majorVersion = 3;
-        contextSettings.minorVersion = 3;
-
-        m_window.create(sf::VideoMode::getDesktopMode(),
-                        "MazeSolver");
-            m_window.display();
 }
 
 void
